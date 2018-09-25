9840f1a40aa4cbc5c8adb3d958d4c4b88f80b3e3
Vanina Mikova <vanina.dzhuteva@gmail.com>
2018-09-25 19:05:27 +0300

    add scroll to top button and refactoring index file

---
+++ build/back-to-top.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+//Get the id of the button
+var scrollBtn = document.getElementById('scrollBtn');
+//Deteck button appearance
+var scrollFunction = function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollBtn.style.display = 'block';
+    } else {
+        scrollBtn.style.display = 'none';
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+var topFunction = function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
+scrollBtn.addEventListener('click', function () {
+    topFunction();
+});
